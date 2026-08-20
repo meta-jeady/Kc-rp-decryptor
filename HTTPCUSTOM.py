68e9d5a1db3db44d60bd29acb64d8791a18ce54f
META JEADY <djiogofoyet@gmail.com>
2026-08-20 11:01:37 +0100

    Update script header and code attribution

--- HTTPCUSTOM.py
+++ HTTPCUSTOM.py
@@ -305,11 +305,11 @@ class HCDecryptor:
             result_dict = {"Protections": protections, "Config": config_data}
 
             return (
-                f"HABIBI HTTP CUSTOM SCRIPT\n"
+                f"KCORP TECH HTTP CUSTOM SCRIPT\n"
                 f"{'='*30}\n\n"
                 f"{json.dumps(result_dict, indent=4, ensure_ascii=False)}\n\n"
                 f"{'='*30}\n"
-                f"code : @HABIBI_1ST"
+                f"code : @KCORP TECH_1ST"
             )
         return None
 
